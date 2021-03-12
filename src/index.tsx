48d3c1d7700dca41d8ef3f68c8613189de38c2fa
unknown <jimmychan018@gmail.com>
2021-03-12 17:55:32 +1100

    added google analytics SEO

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-191970802-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
