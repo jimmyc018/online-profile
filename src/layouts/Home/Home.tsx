4bbc38133cc1569519f47a335b96a8312c3e33e9
unknown <jimmychan018@gmail.com>
2021-03-30 21:30:11 +1100

    disabled button

--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -51,7 +51,7 @@ const Home = () => (
                 <span className="tag">UX Research</span>
                 <span className="tag">Health Care</span>
               </div>
-              <Link className="button " to="/ProjectOne" type="button">Coming Soon</Link>
+              <Link className="button disabled-link" to="/ProjectOne" type="button">Coming Soon</Link>
             </div>
 
             {/* <div>
